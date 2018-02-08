d6162bb0e9d78ff5f80110264952427f7bd873b8
shajaroh <shajaroh@gmail.com>
2018-02-07 16:05:02 -0800

    Creates initial simple get request handler

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,30 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var http = require('./http-helpers.js');
+var fs = require('fs');
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  console.log('Serving request type ' + req.method + ' for url ' + req.url);
+
+  var headers = http.headers;
+  var statusCode = 404;
+  
+
+  if (req.method === 'GET') {
+    var content = path.join(__dirname, '/public/index.html');
+    fs.readFile(content, (err, data) => {
+      if (err) throw err;
+      console.log(data);
+      statusCode = 200;
+      res.writeHead(statusCode, 'Roger, Roger', headers); 
+      res.end(data);     
+    });
+    
+    
+    
+  }
+
+  //res.end(archive.paths.list);
+  //res.end();
 };
